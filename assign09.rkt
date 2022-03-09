fca503120866d8b6703524e0c1b37c64a5d23a2d
n8-ball <nateball17@gmail.com>
2022-03-09 14:03:59 -0800

    added ExprC and Value definitions

--- assign09.rkt
+++ assign09.rkt
@@ -1,1 +1,22 @@
 #lang typed/racket
+
+(require typed/rackunit)
+
+;; ===== Defintions =====
+
+;; -- ExprC --
+(define-type ExprC (U errorC voidC numC idC strC boolC))
+(struct errorC ([id : Real]) #:transparent) ;; For inducing errors
+(struct voidC ()) ;; Void
+(struct numC ([n : Real]) #:transparent) ;; Number
+(struct idC ([i : Symbol]) #:transparent) ;; ID
+(struct strC ([s : String]) #:transparent) ;; String
+(struct boolC ([b : Boolean]) #:transparent) ;; Boolean
+(struct appC ([name : Symbol] [args : (Listof ExprC)])) ;; Function call
+(struct defC ([name : Symbol] [args : (Listof Symbol)] [body : ExprC])) ;; Function definition
+;;(struct defPC)
+
+;; -- Value --
+(define-type Value (U errV Number Real Boolean Symbol String primOp))
+(struct errV ([id : Real]) #:transparent) ;; For inducing errors
+(struct primOp ([op : (-> (Listof Any) Value)])) ;; PrimOp
